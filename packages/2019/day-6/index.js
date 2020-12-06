96df1b53327823b1c10cf923088c709f38744344
Antoine <antoine395.caron@gmail.com>
2020-12-06 12:44:40 +0100

    handle husky hooks

--- packages/2019/day-6/index.js
+++ packages/2019/day-6/index.js
@@ -2,43 +2,43 @@ const fs = require("fs");
 const util = require("util");
 const read = util.promisify(fs.readFile);
 
-
-
-
 const run = async () => {
-  const file = await read('./data.txt', "utf8")
-  const starMap = file.split('\n').reduce((map, line) => {
-    map[line.split(')')[1]] = line.split(')')[0];
+  const file = await read("./data.txt", "utf8");
+  const starMap = file.split("\n").reduce((map, line) => {
+    map[line.split(")")[1]] = line.split(")")[0];
     return map;
   }, {});
 
-  const getAncestorCount = body => body in starMap ? 1 + getAncestorCount(starMap[body]) : 0;
+  const getAncestorCount = (body) =>
+    body in starMap ? 1 + getAncestorCount(starMap[body]) : 0;
 
-  console.log(Object.keys(starMap).reduce((sum, body) => sum + getAncestorCount(body), 0));
-}
-
+  console.log(
+    Object.keys(starMap).reduce((sum, body) => sum + getAncestorCount(body), 0)
+  );
+};
 
 const part2 = async () => {
-  const file = await read('./data.txt', "utf8")
-  const starMap = file.split('\n').reduce((map, line) => {
-    map[line.split(')')[1]] = line.split(')')[0];
+  const file = await read("./data.txt", "utf8");
+  const starMap = file.split("\n").reduce((map, line) => {
+    map[line.split(")")[1]] = line.split(")")[0];
     return map;
   }, {});
 
-  const getAncestors = body => body in starMap ? [ ...getAncestors(starMap[body]), starMap[body] ] : [];
+  const getAncestors = (body) =>
+    body in starMap ? [...getAncestors(starMap[body]), starMap[body]] : [];
 
-  const youAncestors = getAncestors('YOU');
-  const santaAncestors = getAncestors('SAN');
+  const youAncestors = getAncestors("YOU");
+  const santaAncestors = getAncestors("SAN");
 
   const transfers = youAncestors
-    .filter(body => santaAncestors.includes(body))
-    .map(body => [
+    .filter((body) => santaAncestors.includes(body))
+    .map((body) => [
       ...youAncestors.slice(youAncestors.indexOf(body)).reverse(),
-      ...santaAncestors.slice(santaAncestors.indexOf(body) + 1)
+      ...santaAncestors.slice(santaAncestors.indexOf(body) + 1),
     ]);
 
-  console.log(Math.min(...transfers.map(transfer => transfer.length - 1)));
-}
+  console.log(Math.min(...transfers.map((transfer) => transfer.length - 1)));
+};
 
 run();
 part2();
--- packages/2020/day-6/index.js
+++ packages/2020/day-6/index.js
@@ -5,39 +5,36 @@ const assert = require("assert");
 const read = util.promisify(fs.readFile);
 
 const loadGroup = async () => {
-  const file = await read('./data.txt', 'utf8');
-  const groups = file
-     .split('\n\n')
-     .map(group => group.split('\n'));
+  const file = await read("./data.txt", "utf8");
+  const groups = file.split("\n\n").map((group) => group.split("\n"));
 
   return groups;
-}
+};
 
 const reduceGroup = (group) => {
-  return _.union(...group.map(person => person.split(''))).length
-}
-
+  return _.union(...group.map((person) => person.split(""))).length;
+};
 
 const run = async () => {
   const groups = await loadGroup();
   let count = 0;
-  for(let i=0;i< groups.length; i++) {
-    count+=reduceGroup(groups[i])
+  for (let i = 0; i < groups.length; i++) {
+    count += reduceGroup(groups[i]);
   }
-  console.log('Total part 1', count)
+  console.log("Total part 1", count);
 };
 
 const intersection = (group) => {
-  return _.intersection(...group.map(person => person.split(''))).length
-}
+  return _.intersection(...group.map((person) => person.split(""))).length;
+};
 
 const part2 = async () => {
   const groups = await loadGroup();
   let count = 0;
-  for(let i=0;i< groups.length; i++) {
-    count+=intersection(groups[i])
+  for (let i = 0; i < groups.length; i++) {
+    count += intersection(groups[i]);
   }
-  console.log('Total part 2', count)
+  console.log("Total part 2", count);
 };
 
 run();
