e6272c3e2c8aa51dba3a0507d7f724e0bb114983
Antoine CARON <antoine.caron@bedrockstreaming.com>
2020-12-11 13:48:00 +0100

    add 2020 day 10 part 2

--- packages/2020/day-11/index.js
+++ packages/2020/day-11/index.js
@@ -115,8 +115,94 @@ const run = async () => {
   console.log("Final occupied seats", countOccupiedSeats(rows));
 };
 
+const nextItem = ({ direction, x, y }) => {
+  if (direction === "top-left") return { y: y - 1, x: x - 1 };
+  if (direction === "top") return { y: y - 1, x };
+  if (direction === "top-right") return { y: y - 1, x: x + 1 };
+  if (direction === "right") return { y, x: x + 1 };
+  if (direction === "bottom-right") return { y: y + 1, x: x + 1 };
+  if (direction === "bottom") return { y: y + 1, x };
+  if (direction === "bottom-left") return { y: y + 1, x: x - 1 };
+  if (direction === "left") return { y, x: x - 1 };
+};
+
+const recursion = ({ rows, direction, x, y }) => {
+  if (!rows[y]) return "";
+  if (!rows[y][x]) return "";
+  if (rows[y][x] !== ".") return rows[y][x];
+  return recursion({ rows, direction, ...nextItem({ direction, x, y }) });
+};
+
+const analyseFullNeighboors = ({ rows, x, y }) => {
+  let count = 0;
+  if (recursion({ rows, x: x - 1, y: y - 1, direction: "top-left" }) === "#") {
+    count++;
+  }
+  if (recursion({ rows, y: y - 1, x, direction: "top" }) === "#") {
+    count++;
+  }
+  if (recursion({ rows, y: y - 1, x: x + 1, direction: "top-right" }) === "#") {
+    count++;
+  }
+  if (recursion({ rows, y, x: x + 1, direction: "right" }) === "#") {
+    count++;
+  }
+  if (
+    recursion({ rows, x: x + 1, y: y + 1, direction: "bottom-right" }) === "#"
+  ) {
+    count++;
+  }
+  if (recursion({ rows, x, y: y + 1, direction: "bottom" }) === "#") {
+    count++;
+  }
+  if (
+    recursion({ rows, x: x - 1, y: y + 1, direction: "bottom-left" }) === "#"
+  ) {
+    count++;
+  }
+  if (recursion({ rows, x: x - 1, y, direction: "left" }) === "#") {
+    count++;
+  }
+  return count;
+};
+
+const printGrid = (grid) => {
+  console.log(grid.map((row) => row.join("")).join("\n") + "\n");
+};
+
 const part2 = async () => {
   const file = await read("./data.txt", "utf8");
+  let rows = file.split("\n").map((row) => row.split(""));
+  const ROW_LENGTH = rows[0].length;
+  const ROW_COUNT = rows.length;
+
+  let countChanges;
+  do {
+    countChanges = 0;
+    const copy = _.cloneDeep(rows);
+    for (let y = 0; y < ROW_COUNT; y++) {
+      for (let x = 0; x < ROW_LENGTH; x++) {
+        const occupiedSeats = analyseFullNeighboors({
+          rows,
+          x,
+          y,
+        });
+
+        if (rows[y][x] === "L" && occupiedSeats === 0) {
+          copy[y][x] = "#";
+          countChanges++;
+        }
+
+        if (rows[y][x] === "#" && occupiedSeats >= 5) {
+          copy[y][x] = "L";
+          countChanges++;
+        }
+      }
+    }
+    rows = copy;
+  } while (countChanges !== 0);
+
+  console.log("Final full occupied seats", countOccupiedSeats(rows));
 };
 
 run();
