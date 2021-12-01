adcee514832fde2e7ec41f3837cd8ce92eb96926
Antoine <antoine395.caron@gmail.com>
2021-12-01 09:00:57 +0100

    handle day 1

--- packages/2021/day-01/index.js
+++ packages/2021/day-01/index.js
@@ -4,9 +4,22 @@ const util = require("util");
 const read = util.promisify(fs.readFile);
 
 const run = async () => {
-  const values = await read("./data.txt", "utf8").then((data) =>
-    data.split("\n")
-  );
+  const values = await read("./data.txt", "utf8").then((data) => {
+    const array = data.split("\n");
+    return array.map((string) => parseInt(string, 10));
+  });
+
+  const countIncrease = values.reduce((accumulator, value, index) => {
+    if (index !== 0) {
+      if (value > values[index - 1]) {
+        accumulator++;
+      }
+    }
+
+    return accumulator;
+  }, 0);
+
+  console.log({ countIncrease });
 };
 
 run();
--- packages/2021/day-01/part2.js
+++ packages/2021/day-01/part2.js
@@ -4,9 +4,21 @@ const util = require("util");
 const read = util.promisify(fs.readFile);
 
 const run = async () => {
-  const values = await read("./data.txt", "utf8").then((data) =>
-    data.split("\n")
-  );
+  const values = await read("./data.txt", "utf8").then((data) => {
+    const array = data.split("\n");
+    return array.map((string) => parseInt(string, 10));
+  });
+
+  let previousSum = values[0] + values[1] + values[2];
+  let countIncrease = 0;
+  for (let i = 3; i < values.length; i++) {
+    const currentSum = previousSum + values[i] - values[i - 3];
+    if (currentSum > previousSum) {
+      countIncrease++;
+    }
+    previousSum = currentSum;
+  }
+  console.log({ countIncrease });
 };
 
 run();
